71e5e8fb870019f1c5383e6e822f7d5faa1515ca
jennyjean <jennyjean8675309@yahoo.com>
2019-01-03 13:32:54 -0500

    Wrote reducer functions for Redux

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
+//Actions for this function have two attributes, type and friend
 export function manageFriends(state, action){
+  switch(action.type){
+    case 'ADD_FRIEND':
+      state.friends.push(action.friend)
+      return { friends: state.friends };
+    case 'REMOVE_FRIEND':
+      let newFriends = state.friends.filter(f => f.id !== action.id)
+      return { friends: newFriends };
+    default:
+      return state;
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,9 @@
+//Actions only have a type attribute for this function
 export function managePresents(state, action){
+  switch(action.type){
+    case "INCREASE":
+      return { numberOfPresents: state.numberOfPresents + 1 }
+    default:
+      return state
+  }
 }
