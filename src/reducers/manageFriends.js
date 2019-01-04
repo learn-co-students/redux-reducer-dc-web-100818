7eb1b8784af0b20871a7ee41a899a714ee30388b
Nathan Daniel Gezahegne <nathandaniel@Nathans-MacBook-Pro.local>
2019-01-04 13:02:07 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
 export function manageFriends(state, action){
+  switch (action.type){
+    case "ADD_FRIEND":
+      return {friends: [...state.friends,action.friend]}
+    case "REMOVE_FRIEND":
+      let jj = state.friends.filter((f)=> f.id !== action.id)
+    
+      return {friends: jj}
+    default:
+      return state;
+    }
 }
