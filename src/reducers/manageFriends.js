f2d83cde2be80f65fd8266b1484e8e604d700a81
Joseph Arias <arias.joseph.a@gmail.com>
2019-01-04 19:19:33 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
 export function manageFriends(state, action){
+  switch(action.type) {
+    case 'ADD_FRIEND':
+      return {
+        friends: [...state.friends, action.friend ]
+      }
+    case 'REMOVE_FRIEND':
+      let filterArr = state.friends.filter(friend => {
+        return friend.id !== action.id
+      })
+      return {
+        friends: filterArr
+      }
+    default: 
+      return state
+  }
 }
